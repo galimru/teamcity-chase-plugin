a337698c44ba4c75e5ffa8b63612931c04364d91
rgalimov <galimov@haulmont.com>
2017-05-31 10:25:10 +0400

    -- added slack integration

--- src/main/java/com/haulmont/teamcity/SlackNotificator.java
+++ src/main/java/com/haulmont/teamcity/SlackNotificator.java
@@ -1,13 +1,17 @@
 package com.haulmont.teamcity;
 
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.webhook.Payload;
 import com.intellij.openapi.diagnostic.Logger;
 import jetbrains.buildServer.notification.NotificatorAdapter;
 import jetbrains.buildServer.notification.NotificatorRegistry;
 import jetbrains.buildServer.serverSide.SRunningBuild;
 import jetbrains.buildServer.serverSide.UserPropertyInfo;
+import jetbrains.buildServer.users.NotificatorPropertyKey;
 import jetbrains.buildServer.users.SUser;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Set;
 
@@ -17,7 +21,7 @@ import java.util.Set;
  */
 public class SlackNotificator extends NotificatorAdapter {
 
-    Logger log = Logger.getInstance(NotificatorAdapter.class.getName());
+    private Logger log = Logger.getInstance(NotificatorAdapter.class.getName());
 
     public final static String SLACK_CHANNEL_PROPERTY = "slack.Channel";
     public final static String SLACK_USERNAME_PROPERTY = "slack.Username";
@@ -47,7 +51,28 @@ public class SlackNotificator extends NotificatorAdapter {
 
     @Override
     public void notifyBuildSuccessful(@NotNull SRunningBuild build, @NotNull Set<SUser> users) {
-        log.info("build successful" + build.getBuildTypeId());
+        log.info("build successful " + build.getBuildTypeId());
+        sendMessage("build successful", users);
+    }
+
+    private void sendMessage(String text, Set<SUser> users) {
+        for(SUser user : users) {
+            String channel = user.getPropertyValue(new NotificatorPropertyKey(getNotificatorType(), SLACK_CHANNEL_PROPERTY));
+            String username = user.getPropertyValue(new NotificatorPropertyKey(getNotificatorType(), SLACK_USERNAME_PROPERTY));
+            String webhook = user.getPropertyValue(new NotificatorPropertyKey(getNotificatorType(), SLACK_WEBHOOK_PROPERTY));
+            Payload payload = Payload.builder()
+                    .channel(channel)
+                    .username(username)
+                    .text(text)
+                    .build();
+
+            Slack slack = Slack.getInstance();
+            try {
+                slack.send(webhook, payload);
+            } catch (IOException e) {
+                log.error("Error on sending message", e);
+            }
+        }
     }
 
 }
